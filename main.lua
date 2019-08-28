1e3960d6710aecb9158fa9bc2e120a7eff934cd9
Higor Nóbrega <higorst.nobrega@gmail.com>
2019-08-28 14:43:32 -0300

    Limitando os lados

--- main.lua
+++ main.lua
@@ -1,17 +1,22 @@
 function love.load()--Cria e atribui as variaveis (setup)
-    x = 0
-    y = 0
+    x = 100
+    y = 100
     carro = love.graphics.newImage("imagens/carro.png")
+    love.graphics.setBackgroundColor(255,255,255)
 end
 
 function love.update()--Se repere
 
     --função keyboard.isDown responde enquanto o botão estiver precionado
     if love.keyboard.isDown("left") then
+        if x ~= 250 then -- limitando os lados
         x = x - 10
+        end
     end
-    if love.keyboard.isDown("right") then
+    if love.keyboard.isDown("right") then --limitando os lados
+        if x ~= 490 then
         x = x + 10
+        end
     end
     if love.keyboard.isDown("up") then
         y = y - 10
