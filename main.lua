4202a38f5b65d355cdc29a6992f848eacf5a3eaf
Higor Nóbrega <higorst.nobrega@gmail.com>
2019-08-27 23:56:00 -0300

    Atualizando a posição do carro com a função update

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
 function love.load()
-    x = 100
-    y = 100
+    x = 0
+    y = 0
     carro = love.graphics.newImage("imagens/carro.png")
+end
+
+function love.update()
+    x = x + 1
+    y = y + 1
 end
 
 function love.draw()
