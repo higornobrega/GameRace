b5d9a84f3b691ba41e4464243cc8ab89c6bf7739
Higor Nóbrega <higorst.nobrega@gmail.com>
2019-08-27 23:49:47 -0300

    Colocando as coordenadas em variaveis

--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
 function love.load()
+    x = 100
+    y = 100
     carro = love.graphics.newImage("imagens/carro.png")
 end
 
 function love.draw()
-    love.graphics.draw(carro, 300,300)
+    love.graphics.draw(carro, x,y)
 end
