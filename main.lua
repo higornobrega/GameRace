f41b715a04d557016256898bd26273b3f086956d
Higor Nóbrega <higorst.nobrega@gmail.com>
2019-08-28 00:08:19 -0300

    Adicionando as movimentações do carro

--- main.lua
+++ main.lua
@@ -1,14 +1,26 @@
-function love.load()
+function love.load()--Cria e atribui as variaveis (setup)
     x = 0
     y = 0
     carro = love.graphics.newImage("imagens/carro.png")
 end
 
-function love.update()
-    x = x + 1
-    y = y + 1
+function love.update()--Se repere
+    
+    if love.keyboard.isDown("left") then
+        x = x - 1
+    end
+    if love.keyboard.isDown("right") then
+        x = x + 1
+    end
+    if love.keyboard.isDown("up") then
+        y = y - 1
+    end
+    if love.keyboard.isDown("down") then
+        y = y + 1
+    end
+    
 end
 
-function love.draw()
+function love.draw()-- Desenha
     love.graphics.draw(carro, x,y)
 end
