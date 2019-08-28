3223ee5b993afa487de9bc58e13142b77b66d01b
Higor Nóbrega <higorst.nobrega@gmail.com>
2019-08-28 00:23:54 -0300

    Modificando velocidade do carro

--- main.lua
+++ main.lua
@@ -7,35 +7,36 @@ end
 function love.update()--Se repere
 
     --função keyboard.isDown responde enquanto o botão estiver precionado
-    --[[if love.keyboard.isDown("left") then
-        x = x - 1
+    if love.keyboard.isDown("left") then
+        x = x - 10
     end
     if love.keyboard.isDown("right") then
-        x = x + 1
+        x = x + 10
     end
     if love.keyboard.isDown("up") then
-        y = y - 1
+        y = y - 10
     end
     if love.keyboard.isDown("down") then
-        y = y + 1
-    end]]
+        y = y + 10
+    end
 
-    function love.keypressed(key)
+    --Função keypresser não responde enquanto o botão estiver precionado (reponde apenas uma vez por precionado)
+    --[[function love.keypressed(key) 
         if key == "left" then
-            x = x - 1
+            x = x - 10
         end
 
         if key == "right" then
-            x = x + 1
+            x = x + 10
         end
         
         if key == "up" then
-            y = y - 1
+            y = y - 10
         end
         if key == "down" then
-            y = y + 1
+            y = y + 10
         end
-    end    
+    end ]]   
 end
 
 function love.draw()-- Desenha
